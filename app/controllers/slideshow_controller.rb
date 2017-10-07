663cc5e946eb896e1c13223a431ec246576b7d57
Naoki Ishimura <mirontest1460@gmail.com>
2017-10-07 17:03:15 +0900

    Fix error handling

--- app/controllers/slideshow_controller.rb
+++ app/controllers/slideshow_controller.rb
@@ -1,29 +1,22 @@
 class SlideshowController < ApplicationController
   def shots
-    require "faraday"
-    require "json"
+    require 'faraday'
+    require 'json'
 
     @image_urls = []
 
-    begin
-
-      client = Faraday.new
-      res = client.get do |req|
-        req.url ENV["DRIBBBLE_URL"]
-        req.headers['Authorization'] = "Bearer #{ENV["DRIBBBLE_TOKEN"]}"
-      end
-
-      body = JSON.parse res.body
-
-      body.each do |b|
-        hidpi_url = b["images"]["hidpi"]
-        @image_urls.push(hidpi_url) if hidpi_url != nil
-      end
-
-    rescue
-      # Whether DRIBBBLE_URL or DRIBBBLE_TOKEN is incorrect or unspecified.
-      
+    client = Faraday.new
+    res = client.get do |req|
+      req.url ENV['DRIBBBLE_URL']
+      req.headers['Authorization'] = "Bearer #{ENV['DRIBBBLE_TOKEN']}"
     end
 
+    if res.status == 200
+      body = JSON.parse res.body
+      body.each do |b|
+        hidpi_url = b['images']['hidpi']
+        @image_urls.push(hidpi_url) unless hidpi_url.nil?
+      end
+    end
   end
 end
